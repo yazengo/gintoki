ceb8527b8b7ee1cfb72279c15723ad28dce26e31
xieran <enliest@qq.com>
2014-08-23 03:36:49 +0800

    fix

--- radio.lua
+++ radio.lua
@@ -6,98 +6,47 @@ emitter_init(M)
 M.vol = 100
 M.info = function () 
 	return {
-		battery=90,
-		volume=M.vol,
-		wifi={ssid="Sugr"},
-		firmware_version="1.0.1",
-		firmware_need_update=true,
+		battery = 90,
+		volume = audio.get_vol(),
+		wifi = {ssid="Sugr"},
+		firmware_version = "1.0.1",
+		firmware_need_update = true,
 	}
 end
 
 M.set_vol = function (vol) 
-	M.vol = vol
 	M.emit('stat_change')
-	return vol
+	return audio.set_vol(vol)
 end
 
 muno = M
 
-local A = {}
+local R = {}
 
-emitter_init(A)
+emitter_init(R)
 
-A.dur = 0
-A.pos = 0
+R.playlist = localmusic
 
-A.stat = 'buffering'
-
-A.play = function (song)
-	if A.stat ~= 'stopped' then
-		clear_timeout(A.timer)
-		info('playing stopped')
-	end
-
-	A.stat = 'buffering'
-	A.dur = song.duration
-
-	info('buffering ' .. song.title)
-	A.emit('stat_change')
-
-	A.timer = set_timeout(function () 
-		A.stat = 'playing'
-		A.ts_start = os.time()
-
-		info('playing ' .. song.title)
-		A.emit('stat_change')
-
-		A.timer = set_timeout(function ()
-			info('done ' .. song.title)
-			A.stat = 'stopped'
-			A.emit('done')
-		end, song.duration*1000)
-
-	end, 1000)
+R.cursong = function ()
+	return R.playlist.cur()
 end
 
-A.next = function ()
-	info('next')
-	clear_timeout(A.timer)
-	A.emit('done')
+R.info = function ()
+	local r = { type='pandora', station='POP' }
+	return table.add(r, R.cursong() or {})
 end
 
-A.pause = function () 
-	if A.stat == 'playing' then 
-		info('pause')
-		A.stat = 'paused'
-		A.emit('stat_change')
-	end
+R.next = function ()
+	info('radio next')
+	R.playlist.next()
+	R.emit('play', R.cursong())
 end
 
-A.resume = function ()
-	if A.stat == 'paused' then
-		info('resume')
-		A.stat = 'playing'
-		A.emit('stat_change')
-	end
+R.prev = function ()
+	info('radio prev')
+	R.playlist.prev()
+	R.emit('play', R.cursong())
 end
 
-A.play_pause_toggle = function ()
-	if A.stat == 'playing' then A.pause() end
-	if A.stat == 'paused' then A.resume() end
-end
-
-A.info = function ()
-	local r = { stat=A.stat }
-	if A.stat == 'playing' then
-		r.duration = A.dur
-		r.position = os.time() - A.ts_start
-	elseif A.stat == 'paused' then
-		r.duration = A.dur
-		r.position = os.time() - A.ts_start
-	elseif A.stat == 'buffering' then
-		r.position = 0
-	end
-	return r
-end
-
+radio = R
 
--- tests/playlist_1.lua
+++ tests/playlist_1.lua
@@ -1,6 +1,19 @@
 
 local P
 
+local urls = {
+	'http://img.xiami.net/images/album/img48/54548/299932.jpg',
+	'http://sfault-avatar.b0.upaiyun.com/143/624/1436242287-1030000000158255_huge128',
+	'http://img.xiami.net/images/album/img94/10594/572641372578067.jpg',
+}
+
+R.songs = {
+	{title='扛把子的光辉照大地', artist='习近平', album='Best of K.B.Z', duration=10, cover_url=urls[1], id='01'},
+	{title='Superstar K.B.Z', artist='S.H.E', album='Essential of K.B.Z', duration=10, cover_url=urls[2], id='02'},
+	{title='唱支山歌给 K.B.Z 听', artist='邓小平', album='K.B.Z Collection 80s-90s', duration=10, cover_url=urls[3], id='03'},
+}
+R.songs_i = 0
+
 P = {}
 
 P.list = {
@@ -11,9 +24,21 @@ P.list = {
 	{},
 }
 
+P.i = 0
+
 -- return song or nil
 P.next = function ()
+	P.i = P.i + 1
+end
+
+P.prev = function ()
+	P.i = P.i - 1
+end
+
+P.cur = function () 
+	return P.list[(P.i+1)%table.maxn(P.list)]
 end
 
 localmusic = P
+
 
