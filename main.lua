4f932ba9332c21e754728a3412d2622684268174
xieran <enliest@qq.com>
2014-09-14 10:12:57 +0800

    remove stdin handler from main
    Change-Id: I8d6ecaadc5ee12425b86011e5b6c6f4c797919e4

--- main.lua
+++ main.lua
@@ -112,35 +112,6 @@ radio.play = function (song)
 	}
 end
 
-local I = {}
-
-I.cmds = {
-	[[ audio.pause_resume_toggle() ]],
-	[[ radio.next() ]],
-}
-
-I.handle = function (line) 
-	if line == '' then
-		I.usage()
-		return
-	end
-	local i = tonumber(line)
-	if i and i >= 1 and i <= table.maxn(I.cmds) then
-		local cmd = I.cmds[i]
-		print('dostring: ' .. cmd)
-		loadstring(cmd)()
-	end
-end
-
-I.usage = function ()
-	print('--- select command ---')
-	for k,v in pairs(I.cmds) do
-		print(k .. ' ' .. v)
-	end
-end
-
-input = I
-
 on_airplay_start = function ()
 	audio.play {
 		url = 'airplay://',
