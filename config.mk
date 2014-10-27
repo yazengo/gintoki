b24f6d40db013bbbbf2bb8e2c26fac69495f1935
xieran <enliest@qq.com>
2014-10-27 22:08:55 +0800

    add config.mk for muno
    Change-Id: I7719cc2c83bd6ec6a150c61ee576754f357e5dee

--- config.mk
+++ config.mk
@@ -1,4 +1,21 @@
 
+sysroot = ../system/fs_compile
+
 cobjs += audio_out_jzcodec.o
-ldflags += 
+
+ldflags += -L$(sysroot)/lib
+ldflags += -L$(sysroot)/lib/uv01022
+
+cflags += -I${sysroot}/include
+cflags += -I${sysroot}/include/uv01022
+
+CC = mipsel-linux-gcc
+exe = server-mips
+
+all: ${exe}
+
+inst-files := tests *.lua testaudios upnpweb bbcradio.json server-loop.sh
+
+cp-minifs-mips: inst-mips
+	tar xvf inst-mips.tar -C ../../../system/minifs/usr/app
 
