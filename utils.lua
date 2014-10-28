34ce26bb45c000784280d0c40afca6b6d50e9c1a
xieran <nnareix@gmail.com>
2014-10-28 12:46:33 +0800

    Add queue in lua.

--- utils.lua
+++ utils.lua
@@ -234,3 +234,43 @@ oncer = function ()
 	}
 end
 
+queue = {}
+
+function queue.new ()
+	return {first = 0, last = -1}
+end
+
+function queue.pushleft (list, value)
+	local first = list.first - 1
+	list.first = first
+	list[first] = value
+end
+
+function queue.pushright (list, value)
+	local last = list.last + 1
+	list.last = last
+	list[last] = value
+end
+
+function queue.popleft (list)
+	local first = list.first
+	if first > list.last then return nil end
+	local value = list[first]
+	list[first] = nil    -- to allow garbage collection
+	list.first = first + 1
+	return value
+end
+
+function queue.popright (list)
+	local last = list.last
+	if list.first > last then return nil end
+	local value = list[last]
+	list[last] = nil     -- to allow garbage collection
+	list.last = last - 1
+	return value
+end
+
+function queue.len (list)
+	return list.last - list.first + 1
+end
+
