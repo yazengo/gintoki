2b28ffffa892f9514990925b1165a6ffafd6e714
xieran <nnareix@gmail.com>
2014-10-30 13:53:15 +0800

    Add makefiles

---
+++ config-jz.mk
@@ -0,0 +1,26 @@
+
+sysroot = ../system/fs_compile
+
+cobjs += audio_out_jzcodec.o inputdev.o
+
+luvmods += inputdev
+
+ldflags += -L$(sysroot)/lib
+ldflags += -L$(sysroot)/lib/uv01022
+
+cflags += -I${sysroot}/include
+cflags += -I${sysroot}/include/uv01022
+
+CC = mipsel-linux-gcc
+exe = server-mips
+
+all: ${exe}
+
+inst-files := tests *.lua testaudios bbcradio.json server-loop.sh
+
+pack: ${exe}
+	tar cf $@.tar ${exe} $(inst-files)
+
+install: pack
+	tar xvf inst-mips.tar -C ../../../system/minifs/usr/app
+
--- config.mk
+++ config.mk
@@ -1,26 +1,4 @@
 
-sysroot = ../system/fs_compile
-
-cobjs += audio_out_jzcodec.o inputdev.o
-
-luvmods += inputdev
-
-ldflags += -L$(sysroot)/lib
-ldflags += -L$(sysroot)/lib/uv01022
-
-cflags += -I${sysroot}/include
-cflags += -I${sysroot}/include/uv01022
-
-CC = mipsel-linux-gcc
-exe = server-mips
-
-all: ${exe}
-
-inst-files := tests *.lua testaudios bbcradio.json server-loop.sh
-
-inst-mips: server-mips
-	tar cf $@.tar server-mips $(inst-files)
-
-cp-minifs-mips: inst-mips
-	tar xvf inst-mips.tar -C ../../../system/minifs/usr/app
+cobjs += audio_out_libao.o
+ldflags += -lao
 
