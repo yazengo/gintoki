7c201016f5247175f0af96f737a16aca538c80d3
xieran <nnareix@gmail.com>
2014-10-30 12:19:50 +0800

    remove unused files

--- localmusic.lua
+++
@@ -1,6 +0,0 @@
-
-require('playlist')
-
-localmusic = playlist {type='local', dirs={'musics', '/mnt/sdcard/musics'}}
-slumbermusic = playlist {type='slumber', dirs={'slumbermusics', '/mnt/sdcard/shumbermusics'}}
-
