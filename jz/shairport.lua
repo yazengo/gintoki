54a5c367968b339fa05ea68788b4a922e6cb872c
xieran <enliest@qq.com>
2014-12-01 23:44:09 +0800

    Using pdirect() in jz/shairport
    Change-Id: I8d3bacdfd1e3a7f932f4a07819ca751b13e72279

--- jz/shairport.lua
+++ jz/shairport.lua
@@ -6,7 +6,11 @@ local S = {}
 S.start = function (handler)
 	tcpsrv('127.0.0.1', 3389, function (r)
 		info('start')
-		handler(r)
+		local sink = pdirect()
+		pipe.copy(r, sink, function (reason)
+			if reason == 'w' then pexec('pkill shairport') end
+		end)
+		handler(sink)
 	end)
 end
 
